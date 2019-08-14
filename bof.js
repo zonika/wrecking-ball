d09f95511e1c999020650d7b95b945982f5d566e
Zoe Chodosh <zonika@users.noreply.github.com>
2019-08-14 14:25:20 -0400

    Create bof.js

---
+++ bof.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.paywalled-content > .row > .print-full-width');
+const article = wrapper.querySelector('.article-locked');
+article.style.maxHeight = 'unset';
+article.style.overflowY = 'auto';
+wrapper.innerHTML = '';
+wrapper.appendChild(article);
