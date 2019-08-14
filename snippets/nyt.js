e08710f5c39d8d53c147de51d151358489bbdae1
Zoe Chodosh <zonika@users.noreply.github.com>
2019-08-14 14:26:28 -0400

    Create nyt.js

---
+++ snippets/nyt.js
@@ -0,0 +1,5 @@
+const popupThing1234 = document.querySelector('#gateway-content');
+const articleThing1234 = document.querySelector('[role="main"] :first-child');
+popupThing1234.parentNode.removeChild(dumbShitPopupThing);
+articleThing1234.style.position = 'relative';
+articleThing1234.removeChild(articleThing1234.lastChild);
