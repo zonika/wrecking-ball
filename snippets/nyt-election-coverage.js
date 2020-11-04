e1fa1609e952531c6df59f32fae8bd850990cdc3
Zoe Chodosh <zonika@users.noreply.github.com>
2020-11-04 11:34:59 -0500

    Create nyt-election-coverage.js

---
+++ snippets/nyt-election-coverage.js
@@ -0,0 +1,4 @@
+const gateway = document.querySelector('#gateway-content');
+const article = document.querySelector('#site-content');
+gateway.parentNode.removeChild(gateway);
+article.style.position = 'relative';
