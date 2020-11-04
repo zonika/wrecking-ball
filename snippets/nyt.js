dba0b9e96f7b45926fcf510bc49c1efb982f635d
Zoe Chodosh <zonika@users.noreply.github.com>
2020-11-04 11:33:34 -0500

    Update nyt.js

--- snippets/nyt.js
+++ snippets/nyt.js
@@ -1,6 +1,5 @@
-// for paywalled articles in the New York Times
-const popupThing1234 = document.querySelector('#gateway-content');
-const articleThing1234 = document.querySelector('[role="main"] :first-child');
-popupThing1234.parentNode.removeChild(dumbShitPopupThing);
-articleThing1234.style.position = 'relative';
-articleThing1234.removeChild(articleThing1234.lastChild);
+const gateway = document.querySelector('#gateway-content');
+const article = document.querySelector('#app :first-child :first-child');
+gateway.parentNode.removeChild(gateway);
+article.style.position = 'relative';
+article.removeChild(article.lastChild);
